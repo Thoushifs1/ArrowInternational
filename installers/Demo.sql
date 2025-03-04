f366eb228749b5c993c9ff0cc89fb6c387e9490a
Thoushifs1 <thoushifahammad1@gmail.com>
2025-03-04 15:15:05 +0530

    Update Demo.sql

--- installers/Demo.sql
+++ installers/Demo.sql
@@ -1,6 +1,8 @@
-SELECT
-    first_name,
-    last_name,
-    department
-FROM employees
-WHERE department = 'Sales';
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department_id INT,
+    salary DECIMAL(10,2),
+    hire_date DATE
+);
