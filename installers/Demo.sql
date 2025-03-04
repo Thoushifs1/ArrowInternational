b90bc81916f8b355b3fd3e05e9888424021da3a8
Thoushifs1 <thoushifahammad1@gmail.com>
2025-03-04 15:24:50 +0530

    Update Demo.sql

--- installers/Demo.sql
+++ installers/Demo.sql
@@ -1,7 +1,1 @@
-CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE,
-    password_hash VARCHAR(255),
-    email VARCHAR(100) UNIQUE,
-    role VARCHAR(20) CHECK (role IN ('Admin', 'User', 'Manager'))
-);
+CREATE SCHEMA TEST1;
